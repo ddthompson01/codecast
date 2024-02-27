bf462f64c233d18f6fe3d1f23f3de97ae38f8cf0
edelt1125 <eric.deltoro@outlook.com>
2024-02-27 11:37:17 -0600

    Added Sidebar functionality

---
+++ code-cast-project/app/(browse)/_components/sidebar/wrapper.js
@@ -0,0 +1,17 @@
+"use client";
+
+import { useSidebar } from "./use-sidebar";
+
+function Wrapper({ children }) {
+  const { collapsed } = useSidebar((state) => state);
+
+  return (
+    <aside className={`fixed left-0 flex flex-col ${collapsed ? 'w-[70px]' : 'w-60'} h-full bg-[#333642] border-r border-[#2D2E35] z-50 p-4`}>
+      <div className="space-y-6">
+        {children}
+      </div>
+    </aside>
+  );
+}
+
+export default Wrapper;
--- code-cast-project/app/(browse)/layout.jsx
+++ code-cast-project/app/(browse)/layout.jsx
@@ -1,11 +1,16 @@
 import { Navbar } from "./_components/navbar";
+import Sidebar from "./_components/sidebar"
+import Container from  "./_components/container"
 
 const BrowseLayout = ({ children }) => {
   return (
     <>
       <Navbar />
       <div className="flex h-full pt-20">
-        {children}
+        <Sidebar/>
+        <Container>
+          {children}
+        </Container>
       </div>
     </>
   );
