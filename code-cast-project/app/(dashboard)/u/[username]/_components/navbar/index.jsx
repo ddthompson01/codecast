7d6c6374195bf885b007e2e30576dd16d899575c
edelt1125 <eric.deltoro@outlook.com>
2024-03-07 16:41:49 -0600

    Updated dashboard page & linked livekit/clerk webhooks

---
+++ code-cast-project/app/(dashboard)/u/[username]/_components/navbar/index.jsx
@@ -0,0 +1,14 @@
+import { Actions } from "./actions";
+
+export const Navbar = () => {
+  return (
+    <nav className="fixed top-0 w-full h-20 z-[49] px-4 lg:px-10 flex justify-between items-center shadow-sm text-white text-xl font-semibold" style={{ backgroundColor: '#131F2F' }}>
+      <div className="ml-6 lg:ml-20"> 
+        CodeCast - A Live Learning Platform for Programmers
+      </div>
+      <div className="flex items-center space-x-4"> 
+        <Actions />
+      </div>
+    </nav>
+  );
+};
